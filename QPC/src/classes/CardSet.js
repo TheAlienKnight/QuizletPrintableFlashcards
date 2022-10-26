f5512e73a09526414dcd43a24f598fbcfeca4096
AlienKnight <88284489+TheAlienKnight@users.noreply.github.com>
2022-10-25 21:59:34 -0400

    add the CardSet class

---
+++ QPC/src/classes/CardSet.js
@@ -0,0 +1,49 @@
+import { randomUUID } from "crypto"
+class CardSet {
+    id
+    cards
+    // Not sure if I will do anything with these values, may discard them later
+    dateCreated
+    dateEdited
+    dateLastViewed
+    // 
+    imported
+    constructor(imported) {
+        this.id = randomUUID()
+        this.cards = []
+        this.dateCreated = new Date().toLocaleDateString()
+        this.dateLastViewed = this.dateCreated
+        this.dateLastEdited = this.dateCreated
+        this.imported = imported
+    }
+    fromJSON(json) {
+        json = JSON.parse(json)
+        this.id = json.id
+        this.cards = json.cards
+        this.dateCreated = json.dateCreated
+        this.imported = json.imported
+    }
+    toJSON() {
+        return {
+            id: this.id,
+            cards: this.cards,
+            dateCreated: this.dateCreated,
+            dateLastViewed: this.dateLastViewed,
+            dateLastEdited: this.dateLastEdited,
+            imported: this.imported
+        }
+    }
+    createCard(term, definition) { 
+        const card = {
+            id: randomUUID(),
+            term: term,
+            definition: definition     
+        }
+        this.cards.push(card)
+        return card;
+    }
+    deleteCard(id) {
+        this.cards = this.cards.filter((card) => card.id != id)
+    }
+}
+export { CardSet }
--- QPC/src/store/index.js
+++ QPC/src/store/index.js
@@ -19,6 +19,9 @@ export default createStore({
         }
       }
     },
+    storage: {
+      cardSets: []
+    }
   },
   getters: {
   },
