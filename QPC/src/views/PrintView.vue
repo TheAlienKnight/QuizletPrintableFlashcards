43aa322f11a07991c71ee2a6dc257e2a04a60042
AlienKnight <88284489+TheAlienKnight@users.noreply.github.com>
2022-10-28 23:19:01 -0400

    add basic flashcard rendering for now

--- QPC/src/views/PrintView.vue
+++ QPC/src/views/PrintView.vue
@@ -1,51 +1,57 @@
 <template>
-    <NavBar></NavBar>
-    <div id="print" @keyup.esc="this.$data.printView = false" :class="[
+    <NavBar v-if="!printView"></NavBar>
+    <div id="print" :class="[
         $store.state.settings.theme,
         $store.state.settings.accessibility.txtgradient,
         $store.state.settings.accessibility.txtstyle,
     ]">
         <br />
-        <div class="container mobile centered" v-if="!this.id">
-            <p class="medium-text">
-                Hey! You should really only be accessing this page from the dashboard, or from a bookmark link with the
-                set
-                id in the url. As of right now, this function is unavailable because the id doesn't exist, or is
-                invalid.
-            </p>
-            <br /><br />
-            <router-link :to="'/dashboard'"><button class="button">Go To Dashboard</button></router-link>
-        </div>
-        <div class="container mobile" v-if="this.id">
-            <h1 class="large-text" style="text-align: center">View/Edit/Print - {{ this.$data.set.title }}</h1>
-            <h4 class="small-text" style="text-align: center">You can directly edit the existing cards, add new cards,
-                or toggle print view, which hides everything but the card set itself, and opens the print window.</h4>
-            <hr>
-            <div class="centered">
-                <p class="tiny-text">Print view can be toggled off by pressing the escape or "esc" key.
+        <div v-if="!printView">
+            <div class="container mobile centered" v-if="!this.id">
+                <p class="medium-text">
+                    Hey! You should really only be accessing this page from the dashboard, or from a bookmark link with
+                    the
+                    set
+                    id in the url. As of right now, this function is unavailable because the id doesn't exist, or is
+                    invalid.
                 </p>
+                <br /><br />
+                <router-link :to="'/dashboard'"><button class="button">Go To Dashboard</button></router-link>
+            </div>
+            <div class="container mobile" v-if="this.id">
+                <h1 class="large-text" style="text-align: center">View/Edit/Print - {{ this.$data.set.title }}</h1>
+                <h4 class="small-text" style="text-align: center">You can directly edit the existing cards, add new
+                    cards,
+                    or toggle print view, which hides everything but the card set itself, and opens the print window.
+                </h4>
+                <hr>
+                <div class="centered">
+                    <br>
+                    <button class="button" @click="this.make4x6PDF()">Print Flashcards (4x6, or 3x5)</button>&nbsp;
+                    &nbsp;
+                    <button class="button"
+                        @click="this.$data.editmode = !this.$data.editmode; this.$data.printMode = 'list'">Toggle Edit
+                        Mode {{ this.$data.editmode ? 'Off' : 'On' }}</button>
+
+                </div>
+                <hr>
+                <CardCard v-for="card in $data.set.cards" :key="card.id" :term="card.term" :definition="card.definition"
+                    :id="card.id" :deleteSelf="deleteCard" :editmode="$data.editmode" :saveEdits="$data.set.saveChanges"
+                    :card="card">
+                </CardCard>
                 <br>
-                <button class="button">Enter Print View</button> &nbsp; &nbsp; <button class="button"
-                    @click="this.$data.editmode = !this.$data.editmode">Toggle Edit
-                    Mode {{ this.$data.editmode ? 'Off' : 'On' }}</button>
+                <div class="centered">
+                    <button class="button">Add Card</button>
+                </div>
 
             </div>
-            <hr>
-            <CardCard v-for="card in $data.set.cards" :key="card.id" :term="card.term" :definition="card.definition"
-                :id="card.id" :deleteSelf="deleteCard" :editmode="$data.editmode" :saveEdits="$data.set.saveChanges" :card="card">
-            </CardCard>
-            <br>
-            <div class="centered">
-                <button class="button">Add Card</button>
-            </div>
-
+            <div class="is-fullheight-20vh"></div>
+            <FooterObject></FooterObject>
         </div>
-        <div class="is-fullheight-60vh"></div>
-        <FooterObject></FooterObject>
     </div>
 </template>
-
 <script>
+import { FlashcardPDF } from "../classes/FlashcardPDF.js";
 import NavBar from "@/components/NavbarMenu.vue";
 import FooterObject from "@/components/FooterObject.vue";
 import CardCard from "@/components/CardCard.vue";
@@ -65,7 +71,8 @@ export default {
         return {
             set: {},
             printView: false,
-            editmode: false
+            editmode: false,
+            printMode: 'list'
         };
     },
     methods: {
@@ -80,7 +87,10 @@ export default {
         deleteCard(id) {
             // it may seem like you'd be able to just pass in $data.set.deleteCard as a prop, but you lose the proper state and it will fail
             this.$data.set.deleteCard(id)
-        }
+        },
+        makePDF() {
+            new FlashcardPDF(this.set).getFile()
+        },
     },
 };
 </script>
